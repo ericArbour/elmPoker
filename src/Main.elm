877d2d91db2165ef89fa00d57ed6b54e98558fa2
Eric Arbour <arboureric@gmail.com>
2018-06-25 22:12:18 -0400

    add ability to randomly create 2 hands with 5 cards each from a series of 10 clicks and deck decreases accordingly

--- src/Main.elm
+++ src/Main.elm
@@ -2,42 +2,69 @@ module Main exposing (main)
 
 import Array exposing (..)
 import CompareHands.CompareHands exposing (compareHands)
+import Deck exposing (deck)
 import Html exposing (..)
 import Html.Events exposing (..)
 import Random exposing (..)
 
 
 type alias Model =
-    { card : String }
+    { current : Int, hand1 : List String, hand2 : List String, deck : Array String, limit : Int }
 
 
 initModel : ( Model, Cmd Msg )
 initModel =
-    ( Model "", Cmd.none )
+    ( Model 1 [] [] deck 5, Cmd.none )
 
 
 type Msg
-    = Draw
-    | NewCard Int
+    = DrawCard
+    | DealCard Int
+
+
+intToCard : Int -> Array String -> String
+intToCard index deck =
+    case Array.get index deck of
+        Just a ->
+            a
+
+        Nothing ->
+            ""
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Draw ->
-            ( model, Random.generate NewCard (Random.int 0 51) )
+        DrawCard ->
+            ( model, Random.generate DealCard (Random.int 0 (Array.length model.deck - 1)) )
 
-        NewCard cardInt ->
-            ( Model
-                (case Array.get cardInt deck of
-                    Just a ->
-                        a
-
-                    Nothing ->
-                        ""
-                )
-            , Cmd.none
-            )
+        DealCard cardInt ->
+            if model.current == 1 then
+                if List.length model.hand1 == model.limit then
+                    ( model, Cmd.none )
+                else
+                    ( { model
+                        | hand1 = intToCard cardInt model.deck :: model.hand1
+                        , hand2 = model.hand2
+                        , deck = Array.filter (\card -> card /= intToCard cardInt model.deck) model.deck
+                        , current = 2
+                      }
+                    , Cmd.none
+                    )
+            else if model.current == 2 then
+                if List.length model.hand2 == model.limit then
+                    ( model, Cmd.none )
+                else
+                    ( { model
+                        | hand1 = model.hand1
+                        , hand2 = intToCard cardInt model.deck :: model.hand2
+                        , deck = Array.filter (\card -> card /= intToCard cardInt model.deck) model.deck
+                        , current = 1
+                      }
+                    , Cmd.none
+                    )
+            else
+                ( model, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -45,91 +72,17 @@ subscriptions model =
     Sub.none
 
 
-deck : Array String
-deck =
-    Array.fromList
-        [ "2H"
-        , "3H"
-        , "4H"
-        , "5H"
-        , "6H"
-        , "7H"
-        , "8H"
-        , "9H"
-        , "TH"
-        , "JH"
-        , "QH"
-        , "KH"
-        , "AH"
-        , "2D"
-        , "3D"
-        , "4D"
-        , "5D"
-        , "6D"
-        , "7D"
-        , "8D"
-        , "9D"
-        , "TD"
-        , "JD"
-        , "QD"
-        , "KD"
-        , "AD"
-        , "2S"
-        , "3S"
-        , "4S"
-        , "5S"
-        , "6S"
-        , "7S"
-        , "8S"
-        , "9S"
-        , "TS"
-        , "JS"
-        , "QS"
-        , "KS"
-        , "AS"
-        , "2C"
-        , "3C"
-        , "4C"
-        , "5C"
-        , "6C"
-        , "7C"
-        , "8C"
-        , "9C"
-        , "TC"
-        , "JC"
-        , "QC"
-        , "KC"
-        , "AC"
-        ]
-
-
-hand1 =
-    [ "AC"
-    , "2C"
-    , "3C"
-    , "4C"
-    , "5C"
-    ]
-
-
-hand2 =
-    [ "AH"
-    , "AD"
-    , "AD"
-    , "AD"
-    , "KH"
-    ]
-
-
+test : Msg
 test =
-    compareHands hand1 hand2
+    DrawCard
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text (toString model.card) ]
-        , button [ onClick Draw ] [ text "Draw " ]
+        [ h1 [] [ text (toString model) ]
+        , button [ onClick test ] [ text "Draw " ]
+        , div [] []
         ]
 
 
